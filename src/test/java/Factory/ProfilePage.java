bf4eeee50d2b6d73057e076fd35dd1300be682b5
alex_ <alex_kr@abv.bg>
2023-02-12 10:33:39 +0200

    next commit

--- src/test/java/Factory/ProfilePage.java
+++ src/test/java/Factory/ProfilePage.java
@@ -1,4 +1,34 @@
 package Factory;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class ProfilePage {
+    public static final String PAGE_URL = "http://training.skillo-bg.com:4200/users/";
+    private final WebDriver driver;
+
+    @FindBy(tagName = "h2")
+    private WebElement userName;
+
+
+    public ProfilePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean isUrlLoaded() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.urlContains(ObjectWebPages.ProfilePage.PAGE_URL));
+    }
+
+    public String getUsername() {
+
+        return userName.getText();
+    }
 }
